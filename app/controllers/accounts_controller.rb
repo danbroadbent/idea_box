8806dcbb46ca47471d7e2a6f1b003092bc54f4bd
Daniel Broadbent <dbroadbent85@gmail.com>
2016-09-10 14:54:37 -0600

    Added strong params to create account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,12 +5,18 @@ def new
 end
 
 def create
-  @account = Account.new
+  @account = Account.create(account_params)
   if @account.save
     redirect_to ideas_path
   else
     render :new
   end
+end
+
+private
+
+def account_params
+  params.require(:account).permit(:username, :password)
 end
 
 end
